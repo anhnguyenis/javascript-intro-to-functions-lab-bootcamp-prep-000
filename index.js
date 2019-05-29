f6939f3e1e5f70dea5250acc5b55ce7d75eb3b60
Anh Nguyen <anhnguyenis@gmail.com>
2019-05-29 21:27:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,9 +2,13 @@ function shout(string) {
  return string.toUpperCase()
 }
 
+shout("hello");
+
 function whisper(string) {
   return string.toLowerCase()
 }
+
+whisper
 
 function logShout(string) {
   console.log('HELLO')
