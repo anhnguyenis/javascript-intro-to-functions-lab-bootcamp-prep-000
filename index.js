cdfc8ef1b56c1d746a0fdbb94c34c99853916ff3
Anh Nguyen <anhnguyenis@gmail.com>
2019-05-29 21:20:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,14 +15,14 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
- if 'string' === string.tolowerCase
+ if string === string.tolowerCase
  return "I can't hear you!";
 }
 {
-  if 'string' === string.toUpperCase
+  if string === string.toUpperCase
   return "YES INDEED";
 }
 { 
-  if 'string' === "I love you, Grandma."
+  if string === "I love you, Grandma."
   return "I love you, too."; 
 }
